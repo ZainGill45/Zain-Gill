92c9a48231244b1b147a15c41fb050ae045ad08a
Zain Gill <zaingill45@gmail.com>
2024-12-27 15:32:40 -0700

    update

--- contact.php
+++ contact.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/x-icon" href="./images/icons/favicon.ico">
+    <meta name="color-scheme" content="light dark" />
     <link rel="stylesheet" href="./css/framework.css">
     <link rel="stylesheet" href="./css/styles.css">
     <title>Home</title>
--- index.php
+++ index.php
@@ -3,8 +3,10 @@
 
 <head>
     <meta charset="UTF-8">
+    <html data-theme="light">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/x-icon" href="./images/icons/favicon.ico">
+    <meta name="color-scheme" content="light dark" />
     <link rel="stylesheet" href="./css/framework.css">
     <link rel="stylesheet" href="./css/styles.css">
     <title>Home</title>
