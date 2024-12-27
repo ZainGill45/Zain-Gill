f7a64613c7d2463d89f7442fc057ceecf90295ac
Zain Gill <zaingill45@gmail.com>
2024-12-27 15:19:11 -0700

    update

--- contact.php
+++ contact.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/x-icon" href="./images/icons/favicon.ico">
-    <link rel="stylesheet" href="./css/pico.min.css">
+    <link rel="stylesheet" href="./css/framework.css">
     <link rel="stylesheet" href="./css/styles.css">
     <title>Home</title>
 </head>
