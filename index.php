843504f5e1055812778022247438bebffd16b3e6
Zain Gill <zaingill45@gmail.com>
2024-12-27 14:56:48 -0700

    update

--- index.php
+++ index.php
@@ -4,12 +4,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/x-icon" href="./images/favicon/favicon.ico">
     <link rel="stylesheet" href="./css/styles.css">
     <title>Home</title>
 </head>
 
 <body>
-    <h1 class="text-center text-7xl">Home Page</h1>
+    <h1>Home Page</h1>
 </body>
 
 </html>
