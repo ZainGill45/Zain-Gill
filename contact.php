0dfa2471b773beaf1eea0bc611d93356d5a47b53
Zain Gill <zaingill45@gmail.com>
2025-01-04 18:48:38 -0700

    update

--- contact.php
+++ contact.php
@@ -5,12 +5,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/x-icon" href="./images/icons/favicon.ico">
-    <link rel="stylesheet" href="./css/styles.css">
+    <link rel="stylesheet" href="./css/base.css">
+    <link rel="stylesheet" href="./css/main.css">
     <title>Home</title>
 </head>
 
 <body>
-    <h1>Contact Page</h1>
+<h1>Contact Page</h1>
 </body>
 
 </html>
