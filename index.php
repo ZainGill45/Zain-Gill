b7ac86f970a6cfbec102d47d7fc68b715d5e738b
Zain Gill <zaingill45@gmail.com>
2025-01-04 18:37:03 -0700

    update

--- index.php
+++ index.php
@@ -5,21 +5,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/x-icon" href="./images/icons/favicon.ico">
-    <link rel="stylesheet" href="./css/styles.css">
+    <link rel="stylesheet" href="./css/base.css">
+    <link rel="stylesheet" href="./css/main.css">
     <title>Home</title>
 </head>
 
 <body>
-    <main>
-        <h1>Sign In</h1>
-        <form>
-            <label for="email">Email</label>
-            <input type="email" id="email" name="email" required>
-            <label for="password">Password</label>
-            <input type="password" id="password" name="password" required>
-            <button type="submit">Sign In</button>
-        </form>
-    </main>
+<h1>Hello World!</h1>
 </body>
 
 </html>
